515173c5ab294862bd0bfbe87844a7e3a05e6b8a
uzielmvuama <uzielmvuama0@gmail.com>
2024-06-11 00:17:02 -0400

    Partial kuser

---
+++ app/(guest)/contact/page.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IContactPageProps {
+}
+
+export default class ContactPage extends React.Component<IContactPageProps> {
+  public render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
