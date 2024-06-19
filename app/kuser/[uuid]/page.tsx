9318d658c0a380fb2bcd82d2434b36e2ca6e34cf
uzielmvuama <uzielmvuama0@gmail.com>
2024-06-19 00:08:08 -0400

    Metadata and Robots set

--- app/kuser/[uuid]/page.tsx
+++ app/kuser/[uuid]/page.tsx
@@ -3,6 +3,7 @@ import {UserService} from "@/app/_core/api/services/UserService";
 import type {Metadata, ResolvingMetadata} from 'next'
 import {ROOT_FILES_URL} from "@/app/_core/config/constants";
 import {ucfirst} from "@/app/_core/utils/functions";
+import KuserLayout from "@/app/kuser/[uuid]/layout";
 
 export async function generateMetadata(
     {params}: { params: { uuid: string } },
@@ -20,11 +21,17 @@ export async function generateMetadata(
 
     return {
         title: "Kuser",
+        twitter:{
+            title:"Kuser - " + ucfirst(kuser.firstname) + " " + ucfirst(kuser.name),
+            card:"summary_large_image",
+            images: ROOT_FILES_URL + "/compressed-photo/" + kuser.uuid + ".jpg",
+            description: ucfirst(kuser.vinfo.note.text),
+        },
         openGraph: {
             url: "https://www.ikonect.me/kuser/" + kuser.uuid,
             title:"Kuser - " + ucfirst(kuser.firstname) + " " + ucfirst(kuser.name),
             siteName: "Konect",
-            description: kuser.vinfo.note.text,
+            description: ucfirst(kuser.vinfo.note.text),
             images: [ROOT_FILES_URL + "/compressed-photo/" + kuser.uuid + ".jpg", ...previousImages],
         },
     }
@@ -34,7 +41,7 @@ export default async function KuserPage({params}: { params: { uuid: string } }) 
     var kuser = (await UserService.getUser(params.uuid)).data;
 
     return (
-        <div>
+        <KuserLayout>
             <KuserBlock kuser={kuser} isLoading={!kuser}/>
-        </div>);
+        </KuserLayout>);
 }
