345b4f865ee840bedcbe5b572bcd98a9307d6971
uzielmvuama <uzielmvuama0@gmail.com>
2024-06-20 20:56:44 -0400

    Login partian

--- app/_core/api/constants.ts
+++ app/_core/api/constants.ts
@@ -1,5 +1,5 @@
-// export const SERVER_URL = 'https://api.konect.network';
-export const SERVER_URL = 'http://localhost:8000';
+export const SERVER_URL = 'https://api.konect.network';
+// export const SERVER_URL = 'http://localhost:8000';
 
 export const SERVER_API_URL = SERVER_URL + "/api";
 
