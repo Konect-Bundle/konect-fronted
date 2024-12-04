9303e45c7975de464571105fb53255f4fb12aea6
Uziel Mvuama <uzielmvuama@gmail.com>
2024-12-04 02:57:04 -0500

    L

--- app/_components/Inc/KuserBlock.tsx
+++ app/_components/Inc/KuserBlock.tsx
@@ -156,7 +156,7 @@ export default function KuserBlock({
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{
-                    duration: 0.2,
+                    duration: 0.4,
                     delay: 0.1,
                     ease: [0, 0.71, 0.2, 1.0],
                 }}
